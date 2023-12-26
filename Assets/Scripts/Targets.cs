d23e3eda5ba8eec7d97fcb0e29e583563468781c
Dennis Hackethal <dh@dennishackethal.com>
2023-12-26 15:12:01 -0700

    Throw prefabs in the air with random torque, force, and x position

--- Assets/Scripts/Targets.cs
+++ Assets/Scripts/Targets.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Targets : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        rb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
     }
 
     // Update is called once per frame

ebeb680c191bc899df21152ec37320e32b08af56
Dennis Hackethal <dh@dennishackethal.com>
2023-12-26 15:18:27 -0700

    Housekeeping

--- Assets/Scripts/Targets.cs
+++ Assets/Scripts/Targets.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class Targets : MonoBehaviour
 {
     private Rigidbody rb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
-        rb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        rb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        rb.AddForce(Vector3.up * RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private float RandomForce()
+    {
+        return Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }

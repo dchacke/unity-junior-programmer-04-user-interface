679eb13d80e4247c4308471ceab36246b4779493
Dennis Hackethal <dh@dennishackethal.com>
2023-12-26 17:51:49 -0700

    Initialize score text and variable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+
+    float spawnRate = 1.0f;
+    int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("SpawnTargets");
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
